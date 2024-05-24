cb092229ab872621fa1091de802456cad59c4065
Maciek Sitkowski <msitkowski94@gmail.com>
2024-05-24 08:09:59 +0200

    use sveltekit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,29 +1,30 @@
 const overridableDefaults = {
-  endOfLine: "lf",
+  endOfLine: 'lf',
   tabWidth: 2,
   printWidth: 80,
   useTabs: false,
-};
+}
 
 export default {
   ...overridableDefaults,
   singleQuote: true,
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   bracketSpacing: false,
   semi: false,
-  trailingComma: "all",
-  proseWrap: "always",
+  trailingComma: 'all',
+  proseWrap: 'always',
   overrides: [
     {
-      files: "*.md",
+      files: '*.md',
       options: {
         printWidth: 120,
       },
     },
+    {files: '*.svelte', options: {parser: 'svelte', printWidth: 100}},
   ],
   plugins: [
-    "prettier-plugin-svelte",
-    "prettier-plugin-packagejson",
-    "prettier-plugin-tailwindcss",
+    'prettier-plugin-svelte',
+    'prettier-plugin-packagejson',
+    'prettier-plugin-tailwindcss',
   ],
-};
+}
